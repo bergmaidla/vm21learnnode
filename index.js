7c0783b1b97e4a9d9ca0cd71abd409a7e72eba08
Berg Maidla <u-488@tptlive.ee>
2023-01-30 15:08:02 +0200

    Add templating

--- index.js
+++ index.js
@@ -2,12 +2,37 @@ const express = require('express');
 const path = require ('path')
 const app = express();
 const port = 3000;
+const nunjucks = require('nunjucks');
+
+nunjucks.configure('views', {
+autoescape:true,
+express:app
+});
 
 app.get('/', (req, res) => {
-//console.log( path.resolve(__dirname,'index.html'));
-  res.sendFile(path.resolve(__dirname,'index.html'));
+  res.render('index.njk');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.njk');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact.njk');
+});
+
+app.get('/gallery', (req, res) => {
+  res.render('gallery.njk');
+});
+
+app.get('/values', (req, res) => {
+  res.render('values.njk');
+});
+
+app.get('/arbuus', (req, res) => {
+  res.render('arbuus.njk');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port http://localhost: ${port}`);
 });
