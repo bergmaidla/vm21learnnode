e7687278106459b0aab2f7e28ea7d467530d8574
Berg Maidla <u-488@tptlive.ee>
2023-03-27 13:24:49 +0300

    add updates and delete

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require ('path')
 const app = express();
 const port = 3000;
 const nunjucks = require('nunjucks');
-const sqlite3 = require(sqlite3);
+const sqlite3 = require("sqlite3");
 const open = require('sqlite').open;
 
 open({
@@ -11,8 +11,8 @@ open({
   driver: sqlite3.Database
 }).then(async db =>{
   const result = await db.all('select * from articles;');
-  consule.log(result);
-});
+  console.log(result);
+
 
 nunjucks.configure('views', {
 autoescape:true,
@@ -64,14 +64,32 @@ app.get('/arbuus', (req, res) => {
   res.render('arbuus.njk');
 });
 
-app.get('/articles', (req, res) => {
-  const result = await db.all('select * from articles;');
-  console.log(result);
+app.get('/articles',async (req, res) => {
+  const articles = await db.all('select * from articles;');
+  console.log(articles);
   res.render('articles.njk', {articles});
 });
 
+app.get('/articles/new', (req, res) => {
+  res.render('newarticle.njk');
+});
 
+app.post('/articles', async (req, res) => {
+  await db.run(`insert into articles (title,body) values('${req.body.title}','${req.body.body}');`);
+  res.redirect ('/articles');
+});
+
+app.get('/articles/edit', async (req, res) => {
+  const article = await db.get(`select * from articles where id=${req.query.id};`);
+  res.render('editarticle.njk', {article});
+});
+
+app.post('/articles/edit', async (req, res) => {
+  await db.run(`uptade articles set title'${req.body.title}', body='${req.body.body}' where id=${req.query.id}`);
+  res.redirect ('/articles');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost: ${port}`);
+});
 });
